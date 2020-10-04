a8859af6edf25683d959f0817275989f3cbaf893
Arnab Pal <arnabpal05@aol.com>
2020-10-04 15:32:05 +0530

    part 2

--- guessinggame.sh
+++ guessinggame.sh
@@ -4,15 +4,14 @@
 function guessinggame {
 	val=$(ls -l | egrep ^- | wc -l)
 
-	echo "how many files are in the current directory"
-
 	while :
 	do
+		echo "how many files are in the current directory"
 		read response
 		if [[ $response -gt $val ]]; then
-			echo "Your guess was either too high"
+			echo "Your guess was too high"
 		elif [[ $response -lt $val ]]; then
-			echo "Your guess was either too low"
+			echo "Your guess was too low"
 		else
 			echo "Congratulations"
 			exit 0
--- makefile
+++ makefile
@@ -1,6 +1,11 @@
 
-README.md:
-	echo "Guessing Game" > README.md
+README.md: guessinggame.sh
+	echo "#Guessing Game" > README.md
+	echo "\n"
 	date >> README.md
+	echo "\n"
 	cat guessinggame.sh | wc -l >> README.md
+
+clean:
+	rm README.md
 
